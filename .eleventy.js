281d583a44a04c081ec33224b2e79a18ada33baf
Bryce Morgan <brycedmorgan@gmail.com>
2026-02-16 10:50:23 -0700

    Fix paths for GitHub Pages subdirectory
    - Added pathPrefix to eleventy config
    - Updated all href and src paths to use url filter
    - CSS, JS, images now load correctly at /edge-energy-site/

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
   
   return {
+    pathPrefix: "/edge-energy-site/",
     dir: {
       input: "src",
       output: "_site",
